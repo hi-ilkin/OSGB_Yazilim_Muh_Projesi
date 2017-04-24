040713c54ea2f342a46bc863ed222e1b3da7582a
Ilkin Huseynli <huseynlilkin@gmail.com>
2017-04-25 01:22:53 +0300

    Hekim Sinifi

---
+++ src/Hekim.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ilkin
+ */
+public class Hekim {
+    
+    
+    public void muayeneEt(){
+    
+    
+    }
+    
+    public boolean aylikSaglikTaramasi(){
+    
+    
+    return false;
+    }
+    
+    public void iseYeniGirisTaramasi(){
+    
+    
+    }
+    
+    
+    public String aylikSaglikRiskiAnalizRaporu(){
+    
+    
+        
+        return "";
+    }
+    
+}
